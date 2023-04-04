89b92d9c99b911e15618d4bfd04f2a5d78f3e131
r-leyshon <richard.leyshon@ons.gov.uk>
2023-04-04 09:20:03 +0100

    feat: All module 2 intermediate funcs implemented as src

--- src/interm/course_funcs_1.py
+++ src/interm/course_funcs_1.py
@@ -1,7 +1,25 @@
-
+"Funcs exemplifying unit 2: intermediate pytest"
 
 def i_want_string(string):
     if isinstance(string, str):
         pass
     else:
         raise TypeError("That's not a string!")
+    
+def num_to_pH(num):
+    """Describes the property of a pH"""
+    # lets always deal with floats
+    if isinstance(num, int):
+        num = float(num)
+    elif not isinstance(num, float):
+        raise ValueError("`num` must be an integer or float.")
+    # neutral
+    if num == 7.0:
+        return "neutral"
+    elif num >= 0.0 and num < 7.0:
+        return "acid"
+    elif num > 7.0 and num <= 14.0:
+        return "alkali"
+    else:
+        return None
+    
--- tests/interm/test_course_funcs_1.py
+++ tests/interm/test_course_funcs_1.py
@@ -1,18 +1,34 @@
 import pytest
 
-from src.interm.course_funcs_1 import (i_want_string)
+from src.interm.course_funcs_1 import (i_want_string, num_to_pH)
 ####Testing Exceptions####
 
+class TestIWantString(object):
+    # The well-trodden route:
+    def test_str_exception_int(self):
+        with pytest.raises(TypeError, match="That's not a string!"):
+            i_want_string(1)
 
-# The well-trodden route:
-def test_str_exception_int():
-    with pytest.raises(TypeError, match="That's not a string!"):
-        i_want_string(1)
+
+    # Alternatively
+    def test_str_exception_bool(self):
+        with pytest.raises(TypeError) as exc:
+            i_want_string(False)
+        assert exc.match("That's not a string!")
 
 
-# Alternatively
-def test_str_exception_bool():
-    with pytest.raises(TypeError) as exc:
-        i_want_string(False)
-    assert exc.match("That's not a string!")
+class TestNumToPH(object):
+    # Test normal values, 2 - 3
+    def test_normal_return_args(self):
+        assert num_to_pH(1) == "acid"
+        assert num_to_pH(14) == "alkali"
+
+
+    def test_special_args(self):
+        assert num_to_pH(7.0) == "neutral"
+
+
+    def test_boundary_args(self):
+        assert num_to_pH(-1) is None
+        assert num_to_pH(14.1) is None
     
